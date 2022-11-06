c85c7f0d834de32362c547527b5c4b43c94347d1
Wruthless <wsgnzls@comcast.net>
2022-11-06 06:20:34 -0500

    Separating out component and adding some styling.

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
+import { ReactDOM } from "react";
+import Todo from "./components/Todo";
 
 function App() {
     return (
         <div>
             <h1>My Todos</h1>
-            <div className="card">
-                <h2>TITLE</h2>
-                <div>
-                    <button className="btn">Delete</button>
-                </div>
-            </div>
+            <Todo />
         </div>
     );
 }
