e19e7c24d51ff969e912f7b968f1f51877bda644
Wruthless <wsgnzls@comcast.net>
2022-11-08 13:28:24 -0500

    Meetup project skeleton.

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,16 +1,5 @@
-import React from "react";
-import { ReactDOM } from "react";
-import Todo from "./components/Todo";
-
 function App() {
-    return (
-        <div>
-            <h1>My Todos</h1>
-            <Todo text='Learn React'/>
-            <Todo text='Master React'/>
-            <Todo text='Explore full react course'/>
-        </div>
-    );
+    return <div></div>;
 }
 
 export default App;
