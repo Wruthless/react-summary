0a9d6d68d1b6a7c403d7528c6fd5c0f4b4506eb8
Wruthless <wsgnzls@comcast.net>
2022-11-17 08:31:25 -0500

    Form submission redirect functionality.

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useNavigate();
+
     function addMeetupHandler(meetupData) {
         fetch(
             "https://react-getting-started-71ea1-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,9 @@ function NewMeetupPage() {
                     "Content-Type": "application/json"
                 },
             }
-        );
+        ).then(() => {
+            history("/", {replace: true})
+        });
     }
 
     return (
