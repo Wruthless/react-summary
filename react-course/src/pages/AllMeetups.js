ab795e6ec098d4181746268d4be671bda1e73382
Wruthless <wsgnzls@comcast.net>
2022-11-19 16:16:36 -0500

    useEffect hook.

--- react-course/src/pages/AllMeetups.js
+++ react-course/src/pages/AllMeetups.js
@@ -1,33 +1,51 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-    {
-        id: "m1",
-        title: "This is a first meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description:
-            "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-    {
-        id: "m2",
-        title: "This is a second meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description:
-            "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-];
-
 function AllMeetupsPage() {
-    fetch("https://react-getting-started-71ea1-default-rtdb.firebaseio.com/meetups.json",)
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-    return <section>
-        <h1>All Meetups</h1>
-        <ul>
-            <MeetupList meetups={DUMMY_DATA} />
-        </ul>
-    </section>;
+    useEffect(() => {
+        setIsLoading(true);
+
+        fetch(
+            "https://react-getting-started-71ea1-default-rtdb.firebaseio.com/meetups.json"
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const meetupsData = [];
+
+                for (const key in data) {
+                    const meetups = {
+                        id: key,
+                        ...data[key],
+                    };
+
+                    meetupsData.push(meetups);
+                }
+                setIsLoading(false);
+                setLoadedMeetups(meetupsData);
+            });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
+    return (
+        <section>
+            <h1>All Meetups</h1>
+            <ul>
+                <MeetupList meetups={loadedMeetups} />
+            </ul>
+        </section>
+    );
 }
 
 export default AllMeetupsPage;
