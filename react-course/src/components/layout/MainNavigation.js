dd0e0787657f510c36f5c873d6cdc18d87eb7bf0
Wruthless <wsgnzls@comcast.net>
2022-11-28 16:51:38 -0500

    Finalizing context.

--- react-course/src/components/layout/MainNavigation.js
+++ react-course/src/components/layout/MainNavigation.js
@@ -1,21 +1,25 @@
-import { Link } from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
+import {useContext} from "react";
 import classes from './MainNavigation.module.css';
+import favoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+    const favoritesCtx = useContext(favoritesContext);
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
             <ul>
                 <li>
                     <Link to={'/'}>All Meetups</Link>
-                </li> 
+                </li>
                 <li>
                     <Link to={'/new-meetup'}>Add New Meetup</Link>
-                </li> 
+                </li>
                 <li>
-                    <Link to={'/favorites'}>My Favorites</Link>
-                </li> 
+                    <Link to={'/favorites'}>My Favorites
+                        <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                    </Link>
+                </li>
             </ul>
         </header>
     )
